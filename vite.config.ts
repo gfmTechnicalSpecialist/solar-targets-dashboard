f051b8d88d2a10a1996451696442b544517c9c47
William McPherson <mcphersons22kay@gmail.com>
2026-03-27 11:41:33 +0200

    CheckPoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore -- no type declarations available
 import obfuscatorPlugin from 'vite-plugin-obfuscator'
 
 // https://vite.dev/config/
