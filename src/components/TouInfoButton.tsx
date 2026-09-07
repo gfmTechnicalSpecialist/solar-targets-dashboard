840a64fe8719e0d489b134d0016e1a6895b7237c
gfmTechnicalSpecialist <william.mcpherson@mmltd.co.za>
2026-09-07 15:54:20 +0200

    TOU schedule Update

--- src/components/TouInfoButton.tsx
+++ src/components/TouInfoButton.tsx
@@ -30,7 +30,7 @@ const HIGH_DEMAND_SCHEDULE: DaySchedule[] = [
     { period: 'Off-Peak', hours: '22:00-06:00' },
   ]},
   { day: 'Saturday', rows: [
-    { period: 'Standard', hours: '07:00-12:00, 16:00-19:00' },
+    { period: 'Standard', hours: '07:00-12:00, 17:00-19:00' },
     { period: 'Off-Peak', hours: 'all other hours' },
   ]},
   { day: 'Sunday', rows: [
